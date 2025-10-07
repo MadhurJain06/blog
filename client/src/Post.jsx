16998867944cdfb35b64306a392a2f2bab900c67
MadhurJain06 <madhurjain.kota@gmail.com>
2025-10-07 18:36:42 +0530

    https://github.com/MadhurJain06/Blog-Post

---
+++ client/src/Post.jsx
@@ -0,0 +1,22 @@
+export default function Post(){
+    return(
+        <div className='post'>
+
+            <div className="image">
+                <img src="https://images.theconversation.com/files/688908/original/file-20250903-86-h4ds6y.jpg?ixlib=rb-4.1.0&rect=0%2C0%2C5000%2C2500&q=50&auto=format&w=1336&h=668&fit=crop&dpr=2" alt="" />
+            </div>
+
+            <div className="texts">
+                <h2>How Dopamine Detox can help you in your daily routine.</h2>
+                <p className="info">
+                    <a className='author'>Madhur Jain</a>
+                    <time>2025-10-12 12:05</time>
+                </p>
+                <p className='summary'>
+                    “Be kind to yourself” is a piece of advice that’s often given to people during difficult times or moments of stress. But for someone who is driven, a perfectionist or facing pressure, the idea of self-compassion can feel uncomfortable. To them, kindness might feel like letting themselves off the hook.
+                </p>
+            </div>
+
+        </div>
+    )
+}
