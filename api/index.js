931d4dab98077e99f185af50753c57f666033d5a
MadhurJain06 <madhurjain.kota@gmail.com>
2025-10-08 16:50:21 +0530

    authorization

--- api/index.js
+++ api/index.js
@@ -3,11 +3,14 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'fbewug3fvnfbi09fs32biufbv98vn4387'
 
-app.use(cors());
+app.use(cors({credentials:true, origin:'http://localhost:5173'}));
 app.use(express.json());
 
 mongoose.connect(
@@ -26,6 +29,23 @@ app.post("/register", async (req, res) => {
     res.status(400).json(e);
   }
 });
+
+app.post("/login", async(req,res)=>{
+  const {username, password} = req.body;
+  const userDoc = await User.findOne({username});
+  const passOK=bcrypt.compareSync(password, userDoc.password);
+  if(passOK){
+    //logged in
+    jwt.sign({username,id:userDoc._id},secret,{},(err,token)=>{
+      if(err) throw err;
+      res.cookie('token',token).json('ok')
+    })
+  }else{
+    //not logged in
+    res.status(400).json('wrong credentials');
+  }
+
+})
 
 app.listen(4000);
 
